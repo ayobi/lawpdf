f7bc373ac0c826bfbda538ece78fa76e4164ae37
ayobi <obrien.aaron.g@gmail.com>
2023-10-26 01:04:23 -0300

    facelift

---
+++ src/PdfChecker.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import cheerio from 'cheerio';
+
+const LatestLink: React.FC = () => {
+  const [latestLink, setLatestLink] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Replace this URL with the webpage you want to scrape
+    const url = 'https://www.sii.cl/normativa_legislacion/circulares/2023/indcir2023.htm';
+
+    fetch(url)
+      .then((response) => response.text())
+      .then((html) => {
+        const $ = cheerio.load(html);
+        const links = $('a'); // Select all <a> elements
+
+        let latestLinkUrl = '';
+        let latestTimestamp = 0;
+
+        links.each((index, element) => {
+          const linkUrl = $(element).attr('href') || '';
+          const timestamp = Date.parse($(element).attr('data-timestamp') || '');
+
+          if (timestamp > latestTimestamp) {
+            latestTimestamp = timestamp;
+            latestLinkUrl = linkUrl;
+          }
+        });
+
+        setLatestLink(latestLinkUrl);
+      })
+      .catch((error) => {
+        console.error('Error fetching and parsing the webpage:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Latest Link:</h2>
+      {latestLink ? (
+        <a href={latestLink} target="_blank" rel="noopener noreferrer">
+          {latestLink}
+        </a>
+      ) : (
+        'Loading...'
+      )}
+    </div>
+  );
+};
+
+export default LatestLink;
